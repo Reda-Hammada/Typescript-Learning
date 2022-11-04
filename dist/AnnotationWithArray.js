4804285272fc7e28dab135850cbd6c5c033d356d
Reda Hammada <hammada.reda.dev@gmail.com>
2022-11-05 00:53:14 +0100

    example of annotation with arrays added

---
+++ dist/AnnotationWithArray.js
@@ -0,0 +1,6 @@
+"use strict";
+let arrayOne = [1, 2, 3, 4];
+let arrayTwo = ['A', 'B', 'C', 'D'];
+let arrayThree = [1, 2, 3, 4, 'A', 'B', 'C', 'D'];
+let arrayFour = [1, 2, 'A', 'B', ['C', 'D'], [true, false]];
+//# sourceMappingURL=AnnotationWithArray.js.map
---
+++ src/AnnotationWithArray.ts
@@ -0,0 +1,7 @@
+let arrayOne: number[] = [1,2,3,4];
+
+let arrayTwo: string[] = ['A','B','C','D'];
+
+let arrayThree: (string | number)[] = [1,2,3,4,'A','B','C','D'];
+
+let arrayFour:(string | number |string[]|boolean[])[] = [1,2,'A','B',['C','D'],[true,false]];
