1d074c2181e4e0880dda5269f38d91d7d947a3e8
Reda Hammada <hammada.reda.dev@gmail.com>
2023-04-06 18:00:51 +0000

    static members example added

--- dist/classAnnotation.js
+++ dist/classAnnotation.js
@@ -1,19 +1,25 @@
 "use strict";
 class Employee {
     constructor(userName, salary) {
-        this.u = userName;
-        this.s = salary;
+        this.userName = userName;
+        this.salary = salary;
+        this.userName = userName;
+        this.salary = salary;
+        Employee.created++;
+        Employee.getCount();
         this.userNameSalary = function () {
-            return `Hello ${this.u} your salary is ${this.s}`;
+            return `Hello ${this.userName} your salary is ${this.salary}`;
         };
     }
+    static getCount() {
+        console.log(`${this.created} Objects created`);
+    }
     sayUserNameSalary() {
-        return `Hello ${this.u} your salary is ${this.s}`;
+        return `Hello ${this.userName} your salary is ${this.salary}`;
     }
 }
+Employee.created = 0;
 let userOne = new Employee('Reda', 8900);
-console.log(userOne.u);
-console.log(userOne.s);
 console.log(userOne.userNameSalary());
 console.log(userOne.sayUserNameSalary());
 //# sourceMappingURL=classAnnotation.js.map
--- src/classAnnotation.ts
+++ src/classAnnotation.ts
@@ -1,27 +1,54 @@
 /**
+ * Class
  * Type Annotations with Class 
+ *  data access modifiers parameters properties 
+ * 
+ * Public 
+ *   All members of a class in typescript are public 
+ *   All public members can accessed anywhere  
+ * 
+ * Private 
+ *     Members are visible only to that class and are not accessible outside the class
+ * 
+ * Protected 
+ *    Same like Private but can be accessed using the deriving class 
+ *   
+ * Typescript is a layer on Top of Javascript 
+ *  It should remove all the annotations and the access modifiers .
+ *  
+ * Static members
  *
 */
 
-class Employee{
-    u:string;
-    s:number;
-    userNameSalary:() =>  string;
-    constructor(userName: string, salary: number){
-        this.u = userName;
-        this.s = salary;
-        this.userNameSalary = function ( ){
-            return `Hello ${this.u} your salary is ${this.s}`
+class Employee  {
+
+  static created: number = 0;
+
+  static getCount(): void {
+    console.log(`${this.created} objects created`);
+  }
+
+  public userNameSalary:() =>  string;
+
+    constructor(private userName: string, private salary: number){
+
+        this.userName = userName;
+        this.salary = salary;
+        Employee.created++;
+        Employee.getCount();
+        this.userNameSalary = function(){
+
+            return `Hello ${this.userName} your salary is ${this.salary}`
         }
     }
-    sayUserNameSalary(): string{
-        return `Hello ${this.u} your salary is ${this.s}`
+   public sayUserNameSalary(): string {
+
+        return `Hello ${this.userName} your salary is ${this.salary}`
 
     }
 }
 
 let userOne = new Employee('Reda',8900)
-console.log(userOne.u);
-console.log(userOne.s);
+
 console.log(userOne.userNameSalary());
 console.log(userOne.sayUserNameSalary());
