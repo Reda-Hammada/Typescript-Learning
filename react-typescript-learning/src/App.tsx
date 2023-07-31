619681136cf0b0aeee54207447f4173e308d7dc8
Reda-Hammada <hammada.reda.dev@gmail.com>
2023-07-31 11:46:58 +0200

    update readme

---
+++ react-typescript-learning/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+import People from './components/People';
+import Status from './components/Status';
+import Heading from './components/Heading';
+import Definition from './components/Definition';
+
+
+function App() {
+  const peopleData = [
+    {
+      firstName:'Reda',
+      lastName:'Hammada',
+    },
+    {
+
+      firstName:'Yassine',
+      lastName:'Hammada',
+
+    },
+  ]
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Greet name='Reda' age={23} profession='Web developer' />
+       <People people={peopleData} />
+       <Status status='Loading' />
+       <Definition>
+        <Heading>
+          <h1>Coding is art</h1>
+        </Heading>
+       </Definition>
+      </header> 
+    </div>
+  );
+}
+
+export default App;
